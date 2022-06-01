75ddafa76df70074f141c7432b112171e9abadf0
kuchkorov <66349743+kuchkorov@users.noreply.github.com>
2022-06-01 17:36:34 +0500

    basketdagi xatolik to'g'irlandi

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -12,25 +12,26 @@ const Shop = () => {
   const [loading, setLoading] = useState(true);
   const [order, setOrder] = useState([]);
   const [isBasketShow, setBasketShow] = useState(false)
-  console.log(order)
+
 
   const addToBasket = (item) => {
-    const itemIndex = order.findIndex(orderItem => orderItem.id === item.id)
+    const itemIndex = order.findIndex(
+      (orderItem) => orderItem.id === item.id);
     if (itemIndex < 0) {
       const newItem = {
         ...item,
-        quantity: 1
-      }
-      setOrder([...order, newItem])
+        quantity: 1,
+      };
+      setOrder([...order, newItem]);
     }else {
       const newOrder = order.map((orderItem, index) => {
         if(index === itemIndex) {
           return {
             ...orderItem,
             quantity: orderItem.quantity + 1
-          }
+          };
         }else {
-          return item
+          return orderItem
         }
       });
       setOrder(newOrder)
